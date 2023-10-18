8d574624cd57fea34b3ca2fd8266aeb8d22f9704
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 17:32:59 -0400

    Decision object added to UserDataLoader seed for testing purposes

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/seed/UserDataLoader.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/seed/UserDataLoader.java
@@ -1,21 +1,32 @@
 package com.example.impactfuldecisions.seed;
 
+import com.example.impactfuldecisions.models.Decision;
 import com.example.impactfuldecisions.models.User;
+import com.example.impactfuldecisions.repository.DecisionRepository;
 import com.example.impactfuldecisions.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.time.Clock;
+import java.time.LocalDate;
 
 @Component
 public class UserDataLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final DecisionRepository decisionRepository;
 
     @Autowired
-    public UserDataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserDataLoader(UserRepository userRepository,
+                          DecisionRepository decisionRepository,
+                          @Lazy PasswordEncoder passwordEncoder)
+    {
         this.userRepository = userRepository;
+        this.decisionRepository = decisionRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -29,5 +40,12 @@ public class UserDataLoader implements CommandLineRunner {
         user1.setEmailAddress("impactfuldecisionsteam@gmail.com");
         user1.setPassword(passwordEncoder.encode("password12345"));
         userRepository.save(user1);
+
+        Decision decision1 = new Decision();
+        decision1.setTitle("Should I go on the London trip?");
+        decision1.setDescription("Deciding whether or not it is a good idea to travel right now.");
+        decision1.setCreationDate(LocalDate.now(Clock.systemDefaultZone()));
+        decision1.setUser(user1);
+        decisionRepository.save(decision1);
     }
 }
