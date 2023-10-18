a9dce567a102e534e926e13ae168857e376f660f
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 17:06:56 -0400

    DecisionExistsException class created in exceptions package

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/exceptions/DecisionExistsException.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/exceptions/DecisionExistsException.java
@@ -1,2 +1,9 @@
-package com.example.impactfuldecisions.exceptions;public class DecisionExistsException {
+package com.example.impactfuldecisions.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DecisionExistsException extends RuntimeException{
+    public DecisionExistsException(String message) {super(message);}
 }
