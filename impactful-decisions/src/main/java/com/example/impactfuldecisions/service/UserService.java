c781f2e66c58c6de4338e022aeb03dbe5682167a
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 15:55:27 -0400

    loginUser method added to UserService class

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/service/UserService.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/service/UserService.java
@@ -3,15 +3,20 @@ package com.example.impactfuldecisions.service;
 
 import com.example.impactfuldecisions.exceptions.UserExistsException;
 import com.example.impactfuldecisions.models.User;
+import com.example.impactfuldecisions.models.request.LoginRequest;
 import com.example.impactfuldecisions.repository.UserRepository;
 import com.example.impactfuldecisions.security.JWTUtils;
+import com.example.impactfuldecisions.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -32,7 +37,6 @@ public class UserService {
     public User findByUserEmailAddress(String emailAddress) {
         return userRepository.findUserByEmailAddress(emailAddress);
     }
-
     /**
      * Register a new user in the system.
      *
@@ -49,4 +53,24 @@ public class UserService {
             throw new UserExistsException("user email address " + userObject.getEmailAddress() + " already exists");
         }
     }
+    /**
+     * Login a user and generate a JWT token upon successful authentication.
+     *
+     * @param loginRequest The login request containing user credentials.
+     * @return An Optional containing the JWT token if authentication is successful, otherwise empty.
+     */
+    public Optional<String> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+
+
 }
