007f875f2d6d491ab7a1a728fdbcddb5c77910aa
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 15:49:11 -0400

    registerUser method refactored to include passwordEncoder

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/service/UserService.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/service/UserService.java
@@ -4,7 +4,11 @@ package com.example.impactfuldecisions.service;
 import com.example.impactfuldecisions.exceptions.UserExistsException;
 import com.example.impactfuldecisions.models.User;
 import com.example.impactfuldecisions.repository.UserRepository;
+import com.example.impactfuldecisions.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -12,12 +16,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
-
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
     }
 
     public User findByUserEmailAddress(String emailAddress) {
@@ -31,9 +40,9 @@ public class UserService {
      * @return The created User object.
      * @throws UserExistsException if the user's email address already exists in the system.
      */
-    public User registerUser(User userObject) {
-        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
-            userObject.setPassword((userObject.getPassword()));
+    public User registerUser(User userObject){
+        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
             userRepository.save(userObject);
             return userObject;
         } else {
