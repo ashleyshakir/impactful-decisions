57715458013585aa2d0cdcd82fb7bcd7b8a592b1
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 17:11:58 -0400

    business logic for creating a decision written

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/service/DecisionService.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/service/DecisionService.java
@@ -1,5 +1,7 @@
 package com.example.impactfuldecisions.service;
 
+import com.example.impactfuldecisions.exceptions.DecisionExistsException;
+import com.example.impactfuldecisions.models.Decision;
 import com.example.impactfuldecisions.repository.DecisionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,15 @@ public class DecisionService {
     @Autowired
     public void setDecisionRepository(DecisionRepository decisionRepository) {
         this.decisionRepository = decisionRepository;
+    }
+
+    // Testing the business logic for creating a decision
+    public Decision createDecision(Decision decisionObject) {
+        if (decisionRepository.findByTitle(decisionObject.getTitle()) != null) {
+            throw new DecisionExistsException("You have already created a decision with the title: " + decisionObject.getTitle());
+        } else {
+            return decisionRepository.save(decisionObject);
+        }
     }
 
 }
