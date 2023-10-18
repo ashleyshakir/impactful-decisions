3bfb4522504003ab14a7e91ac555a74d48be32ec
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 17:06:30 -0400

    findByTitle method added to DecisionRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/DecisionRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/DecisionRepository.java
@@ -4,5 +4,5 @@ import com.example.impactfuldecisions.models.Decision;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DecisionRepository extends JpaRepository<Decision,Long> {
-
+    Decision findByTitle(String title);
 }
