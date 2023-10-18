64cef5f283a453b0db2296a92b4b519a1ba61482
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 16:55:42 -0400

    DecisionControllerTestDefs class created

---
+++ impactful-decisions/src/test/java/definitions/DecisionControllerTestDefs.java
@@ -0,0 +1,11 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class DecisionControllerTestDefs extends TestSetUpDefs{
+
+    @Given("I am logged into my account securely")
+    public void iAmLoggedIntoMyAccountSecurely() {
+
+    }
+}
