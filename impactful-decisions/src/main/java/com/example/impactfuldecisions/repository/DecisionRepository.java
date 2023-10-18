d3fbe9018bdc9542b6ad3d2ddb02588d7d12dd41
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 17:38:25 -0400

    findByUserId method added to DecisionRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/DecisionRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/DecisionRepository.java
@@ -3,7 +3,10 @@ package com.example.impactfuldecisions.repository;
 import com.example.impactfuldecisions.models.Decision;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DecisionRepository extends JpaRepository<Decision,Long> {
     Decision findByTitle(String title);
     Decision findByIdAndUserId(Long decisionId, Long userId);
+    List<Decision> findByUserId(Long userId);
 }
