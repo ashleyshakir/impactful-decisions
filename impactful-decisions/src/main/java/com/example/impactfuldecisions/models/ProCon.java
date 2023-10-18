ecf4df379ebbafc531fdc4a9235da6dda04792dd
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 15:25:13 -0400

    models created and connected

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/models/ProCon.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/models/ProCon.java
@@ -1,2 +1,106 @@
-package com.example.impactfuldecisions.models;public class ProCon {
+package com.example.impactfuldecisions.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "procons")
+public class ProCon {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String type;
+
+    @Column
+    private Double rating;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "option_id")
+    private Option option;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "criteria_id")
+    private Criteria criteria;
+
+    public ProCon() {
+    }
+
+    public ProCon(Long id, String type, Double rating, String description, Option option, Criteria criteria) {
+        this.id = id;
+        this.type = type;
+        this.rating = rating;
+        this.description = description;
+        this.option = option;
+        this.criteria = criteria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type.toLowerCase();
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public void setOption(Option option) {
+        this.option = option;
+    }
+
+    public Criteria getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(Criteria criteria) {
+        this.criteria = criteria;
+    }
+
+    @Override
+    public String toString() {
+        return "ProCon{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                ", option=" + option +
+                ", criteria=" + criteria +
+                '}';
+    }
 }
--- impactful-decisions/src/main/java/com/example/impactfuldecisions/models/User.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/models/User.java
@@ -1,5 +1,90 @@
 package com.example.impactfuldecisions.models;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column(unique = true)
+    private String emailAddress;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Decision> decisionList;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String emailAddress, String password) {
+        this.id = id;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Decision> getDecisionList() {
+        return decisionList;
+    }
+
+    public void setDecisionList(List<Decision> decisionList) {
+        this.decisionList = decisionList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
 }
