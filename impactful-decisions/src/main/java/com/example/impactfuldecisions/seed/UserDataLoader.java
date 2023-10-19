74d3b35107994ae6575b12bbc879ce5ad60b5dda
ashleyshakir <ashleymshakir@gmail.com>
2023-10-19 11:35:40 -0400

    Pros and Cons added to UserDataLoader seed class

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/seed/UserDataLoader.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/seed/UserDataLoader.java
@@ -1,13 +1,7 @@
 package com.example.impactfuldecisions.seed;
 
-import com.example.impactfuldecisions.models.Criteria;
-import com.example.impactfuldecisions.models.Decision;
-import com.example.impactfuldecisions.models.Option;
-import com.example.impactfuldecisions.models.User;
-import com.example.impactfuldecisions.repository.CriteriaRepository;
-import com.example.impactfuldecisions.repository.DecisionRepository;
-import com.example.impactfuldecisions.repository.OptionRepository;
-import com.example.impactfuldecisions.repository.UserRepository;
+import com.example.impactfuldecisions.models.*;
+import com.example.impactfuldecisions.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Lazy;
@@ -25,18 +19,21 @@ public class UserDataLoader implements CommandLineRunner {
     private final DecisionRepository decisionRepository;
     private final CriteriaRepository criteriaRepository;
     private final OptionRepository optionRepository;
+    private final ProConRepository proConRepository;
 
     @Autowired
     public UserDataLoader(UserRepository userRepository,
                           DecisionRepository decisionRepository,
                           CriteriaRepository criteriaRepository,
                           OptionRepository optionRepository,
+                          ProConRepository proConRepository,
                           @Lazy PasswordEncoder passwordEncoder)
     {
         this.userRepository = userRepository;
         this.decisionRepository = decisionRepository;
         this.criteriaRepository = criteriaRepository;
         this.optionRepository = optionRepository;
+        this.proConRepository = proConRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -85,5 +82,53 @@ public class UserDataLoader implements CommandLineRunner {
         option2.setName("Stay home");
         option2.setDecision(decision1);
         optionRepository.save(option2);
+
+        ProCon proCon1 = new ProCon();
+        proCon1.setType("pro");
+        proCon1.setOption(option1);
+        proCon1.setCriteria(criteria2);
+        proCon1.setDescription("Getting to spend more time with family I don't see a lot");
+        proCon1.setRating(6.0);
+        proConRepository.save(proCon1);
+
+        ProCon proCon2 = new ProCon();
+        proCon2.setType("con");
+        proCon2.setOption(option1);
+        proCon2.setCriteria(criteria1);
+        proCon2.setDescription("It is so expensive to travel over seas");
+        proCon2.setRating(8.0);
+        proConRepository.save(proCon2);
+
+        ProCon proCon3 = new ProCon();
+        proCon3.setType("con");
+        proCon3.setOption(option1);
+        proCon3.setCriteria(criteria3);
+        proCon3.setDescription("I will be exhausted because the 5 hour time difference");
+        proCon3.setRating(9.0);
+        proConRepository.save(proCon3);
+
+        ProCon proCon4 = new ProCon();
+        proCon4.setType("con");
+        proCon4.setOption(option2);
+        proCon4.setCriteria(criteria2);
+        proCon4.setDescription("I won't get to make new memories with the family");
+        proCon4.setRating(5.0);
+        proConRepository.save(proCon4);
+
+        ProCon proCon5 = new ProCon();
+        proCon5.setType("pro");
+        proCon5.setOption(option2);
+        proCon5.setCriteria(criteria1);
+        proCon5.setDescription("I will save so much money if I don't go");
+        proCon5.setRating(9.0);
+        proConRepository.save(proCon5);
+
+        ProCon proCon6 = new ProCon();
+        proCon6.setType("pro");
+        proCon6.setOption(option2);
+        proCon6.setCriteria(criteria3);
+        proCon6.setDescription("I will get normal sleep and be able to function at 100 percent!");
+        proCon6.setRating(7.0);
+        proConRepository.save(proCon6);
     }
 }
