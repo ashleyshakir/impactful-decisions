ba38bf1d0c238c3138f34e0b1184ddd7e2b1c457
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 22:06:26 -0400

    findByName method added to OptionRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/OptionRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/OptionRepository.java
@@ -4,5 +4,6 @@ import com.example.impactfuldecisions.models.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
+    Option findByName (String name);
 
 }
