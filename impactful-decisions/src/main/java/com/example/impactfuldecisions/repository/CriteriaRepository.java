6127dde86f1d4c4a91c4cc3fc22ba5836b5f054d
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 21:34:38 -0400

    findByName method added to CriteriaRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/CriteriaRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/CriteriaRepository.java
@@ -4,5 +4,5 @@ import com.example.impactfuldecisions.models.Criteria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CriteriaRepository extends JpaRepository<Criteria, Long> {
-
+    Criteria findByName (String name);
 }
