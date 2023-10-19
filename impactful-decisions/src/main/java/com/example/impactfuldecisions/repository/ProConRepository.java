6d825b20a639627e790c1f304617606afd5500e6
ashleyshakir <ashleymshakir@gmail.com>
2023-10-19 08:59:53 -0400

    ProConRepository interface created

---
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/ProConRepository.java
@@ -0,0 +1,8 @@
+package com.example.impactfuldecisions.repository;
+
+import com.example.impactfuldecisions.models.ProCon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProConRepository extends JpaRepository<ProCon, Long> {
+
+}
