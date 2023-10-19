8e2e0646ee71185a23fe2e9d41da4b5c2153b9bd
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 21:42:34 -0400

    findByDecisionId method added to CriteriaRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/CriteriaRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/CriteriaRepository.java
@@ -3,6 +3,9 @@ package com.example.impactfuldecisions.repository;
 import com.example.impactfuldecisions.models.Criteria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CriteriaRepository extends JpaRepository<Criteria, Long> {
     Criteria findByName (String name);
+    List<Criteria> findByDecisionId(Long decisionId);
 }
