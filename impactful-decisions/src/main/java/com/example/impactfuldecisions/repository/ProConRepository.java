38b4b3c69775144144a4ee62b2931c1ecdf5e884
ashleyshakir <ashleymshakir@gmail.com>
2023-10-19 09:05:44 -0400

    findByType method added to ProConRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/ProConRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/ProConRepository.java
@@ -4,5 +4,5 @@ import com.example.impactfuldecisions.models.ProCon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProConRepository extends JpaRepository<ProCon, Long> {
-
+    ProCon findByType(String type);
 }
