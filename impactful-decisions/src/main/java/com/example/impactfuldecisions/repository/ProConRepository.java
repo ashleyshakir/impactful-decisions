dbe820e2c5edd8aeff36a1ebf4c7dde499156eb7
ashleyshakir <ashleymshakir@gmail.com>
2023-10-19 09:11:01 -0400

    findByOptionId method added to ProConRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/ProConRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/ProConRepository.java
@@ -3,6 +3,9 @@ package com.example.impactfuldecisions.repository;
 import com.example.impactfuldecisions.models.ProCon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProConRepository extends JpaRepository<ProCon, Long> {
     ProCon findByType(String type);
+    List<ProCon> findByOptionId(Long optionId);
 }
