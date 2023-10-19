3f9455832799f772ebc008e2557247e2dd0ce9ab
ashleyshakir <ashleymshakir@gmail.com>
2023-10-18 22:11:19 -0400

    findByDecisionId method added to OptionRepository

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/OptionRepository.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/repository/OptionRepository.java
@@ -3,7 +3,10 @@ package com.example.impactfuldecisions.repository;
 import com.example.impactfuldecisions.models.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OptionRepository extends JpaRepository<Option, Long> {
     Option findByName (String name);
+    List<Option> findByDecisionId(Long decisionId);
 
 }
