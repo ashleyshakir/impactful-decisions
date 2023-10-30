9d380470dbf6c41ff49ee7e96d719c0badff0d26
ashleyshakir <ashleymshakir@gmail.com>
2023-10-30 19:24:48 -0400

    make it better

--- impactful-decisions/src/main/java/com/example/impactfuldecisions/security/SecurityConfiguration.java
+++ impactful-decisions/src/main/java/com/example/impactfuldecisions/security/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration {
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/auth/users/hello/").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Set session creation policy to STATELESS.
